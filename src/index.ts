db8bafcf0d8e91709b4e20d1472faed4abbbcbc5
Varun Raj <varunraj82@gmail.com>
2021-01-07 10:34:28 -0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,15 @@
 // this common module.
 
 
-interface Color {
-    red: number,
-    blue: number,
-    green : number
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error'
+export * from './errors/database-connection-error'
+export * from './errors/not-authorized-error'
+export * from './errors/not-found-error'
+export * from './errors/request-validation-error'
 
-const color: Color = {
-    red: 20,
-    blue: 10,
-    green: 10
-}
+export * from './middlewares/current-user'
+export * from './middlewares/error-handler'
+export * from './middlewares/require-auth'
+export * from './middlewares/validate-requests'
 
-console.log(color);
-
-export default color;
